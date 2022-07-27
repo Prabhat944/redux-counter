030e6103dbd6e5dbcf9c0778313959f590a87eeb
Prabhat <prabhatkumar944@gmail.com>
2022-07-27 23:58:01 +0530

    increment and decrement by 2 using dispatch

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import classes from './Counter.module.css';
+import {useSelector,useDispatch} from 'react-redux';
+
+const Counter = () => {
+  const dispatch=useDispatch();
+  const counter=useSelector(state=>state.counter);
+  const IncrementHandler=()=>{
+    dispatch({type:'increment'});
+  };
+  const DecrementHandler=()=>{
+    dispatch({type:'decrement'});
+  };
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={DecrementHandler}>Decrement</button>
+        <button onClick={IncrementHandler}>Increment</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
